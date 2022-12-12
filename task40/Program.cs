1ec3251c61785796b6b13b43efd4bf014ee938fe
stanislav <79920025475@yandex.ru>
2022-12-12 08:52:17 +0500

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,13 @@
+﻿//task40
+
+Console.Write("Введите колличество чисел: ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
